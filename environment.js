281a44c4c53e8d23f518c6929b1b0f6a19799ee5
John Larsen <steve@bluestamptravel.com>
2025-08-24 12:01:46 +0100

    Make internals private

--- environment.js
+++ environment.js
@@ -1,75 +1,59 @@
 export default class Environment {
-   scanner = null;
-   parser = null;
-   fnBuilder = null;
-   stateBuilder = null;
-   state = {};
-   fns = [];
-   updateState = null;
-   commands = {};
+   #scanner = null;
+   #parser = null;
+   #fnBuilder = null;
+   #stateBuilder = null;
+   #state = {};
+   #fns = [];
+   #updateState = null;
+   #commands = {};
 
    constructor(scanner, parser, stateBuilder, fnBuilder) {
-      this.scanner = scanner;
-      this.parser = parser;
-      this.stateBuilder = stateBuilder;
-      this.fnBuilder = fnBuilder;
+      this.#scanner = scanner;
+      this.#parser = parser;
+      this.#stateBuilder = stateBuilder;
+      this.#fnBuilder = fnBuilder;
    }
 
+   #getFunction(expr) {
+      const tokens = this.#scanner.scan(expr);
+      const exprTree = this.#parser.parse(tokens);
+      const deps = tokens.filter(t => t.type === "identifier").map(t => t.text);
+      const fn = this.#fnBuilder.getFn(exprTree);
+      return { deps, fn: () => fn(this.#state) };
+   }
+
+   #updateBuilder(shouldReset) {
+      this.#updateState = this.#stateBuilder(this.#fns, shouldReset ? {} : this.#state);
+   }
 
    evaluate(patch = {}) {
-      this.state = this.updateState(patch).newValues;
-      console.log(this.state);
+      this.#state = this.#updateState(patch).newValues;
+      console.log(this.#state);
    }
 
    defineValue(key, expr) {
-      const tokens = this.scanner.scan(expr);
-      const exprTree = this.parser.parse(tokens);
-      const deps = tokens.filter(t => t.type === "identifier").map(t => t.text);
-      const fn = this.fnBuilder.getFn(exprTree);
-
-      this.fns.push({
-         key,
-         deps,
-         fn,
-      });
-
-      this.updateState = this.stateBuilder(this.fns, this.state);
+      const { deps, fn } = this.#getFunction(expr);
+      this.#fns.push({ key, deps, fn });
+      this.#updateBuilder();
       this.evaluate();
    }
 
    defineCommand(command, key, expr) {
-      const tokens = this.scanner.scan(expr);
-      const exprTree = this.parser.parse(tokens);
-      this.commands[command] = {
-         key,
-         fn: this.fnBuilder.getFn(exprTree),
-      };
+      this.#commands[command] = { key, fn: this.#getFunction(expr).fn };
    }
 
    doCommand(command) {
-      const cmd = this.commands[command];
-      if (cmd) {
-         console.log(`Command: ${command}`);
-         const value = cmd.fn(this.state);
-         this.evaluate({ [cmd.key]: value });
-      }
+      const cmd = this.#commands[command];
+      if (cmd) this.evaluate({ [cmd.key]: cmd.fn() });
    }
 
    update(key, expr) {
-      let patch = {};
-      if (key && expr) {
-         const tokens = this.scanner.scan(expr);
-         const exprTree = this.parser.parse(tokens);
-         const fn = this.fnBuilder.getFn(exprTree);
-         const value = fn(this.state);
-         patch = { [key]: value };
-         console.log(`${key} set to ${value}`);
-      }
-      this.evaluate(patch);
+      this.evaluate({ [key]: this.#getFunction(expr).fn() });
    }
 
    reset() {
-      this.updateState = this.stateBuilder(this.fns, {});
+      this.#updateBuilder(true);
       this.evaluate();
    }
 }
